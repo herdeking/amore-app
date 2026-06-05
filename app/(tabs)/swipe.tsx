2e7991ebc9a92fb77564507633112642cfe1d13d
herdeking <your@email.com>
2026-06-05 07:32:34 +0100

    fix: safe swipe screen

--- app/(tabs)/swipe.tsx
+++ app/(tabs)/swipe.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Modal, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Swipe() {
